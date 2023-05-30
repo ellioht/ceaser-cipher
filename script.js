b578dfa2a805f9f13037bbaa58f0eed9dd1fea6d
Elliot <130664947+ellioht@users.noreply.github.com>
2023-05-30 16:46:54 +0100

    rolling cipger with 2 keys

--- script.js
+++ script.js
@@ -4,45 +4,53 @@ const alphabet = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n
 
 let encodeFlag = true; // Variable to track encode/decode state
 
-// Called when button pressed
+// Called when the Encode/Decode button is pressed
 function toggleEncodeDecode() {
     encodeFlag = !encodeFlag; // Toggle the encodeFlag between true and false
     enDeCode(); // Call the enDeCode function to update the input
 }
 
-// Print message
+// Encodes or decodes the user message based on the current encodeFlag value
 function enDeCode() {
     let userString = document.getElementById("userMessage");
-    let userKey = document.getElementById("userKey").value;
+    let userKey1 = document.getElementById("userKey1").value;
+    let userKey2 = document.getElementById("userKey2").value;
     let cleanString = userString.value.trim().toLowerCase();
     let outputMessage = [];
 
     for (let i = 0; i < cleanString.length; i++) {
-        outputMessage.push(codeLetter(cleanString[i], userKey));
+        outputMessage.push(codeLetter(cleanString[i], userKey1, userKey2, i));
     }
 
     userString.value = outputMessage.join("");
 }
 
+// Converts an index to a letter from the alphabet array
 function convertIndexToLetter(index) {
     let letter = alphabet[index];
     return letter;
 }
 
+// Converts a letter to its corresponding index in the alphabet array
 function convertLetterToIndex(letter) {
     let index = alphabet.indexOf(letter);
     return index;
 }
 
-function calculateNewIndex(letter, userKey, encode) {
+// Calculates the new index of a letter based on the userKey1, userKey2, and encode flag
+function calculateNewIndex(letter, userKey1, userKey2, encode, currentIndex) {
     let index = Number(convertLetterToIndex(letter));
-    encode ? (index = index + Number(userKey)) : (index = index - Number(userKey));
+    let totalShift = Number(userKey1) + Number(userKey2) * currentIndex;
+    encode ? (index = index + totalShift) : (index = index - totalShift);
     index = index > 25 ? index - 26 : index < 0 ? index + 26 : index;
     return index;
 }
 
-// Check if non letter
-function codeLetter(letter, userKey) {
+
+// Encodes or decodes a letter based on the userKey1, userKey2, and encode flag
+function codeLetter(letter, userKey1, userKey2, currentIndex) {
     let letterRegEx = /[^a-z]/;
-    return letterRegEx.test(letter) ? letter : convertIndexToLetter(calculateNewIndex(letter, userKey, encodeFlag));
+    return letterRegEx.test(letter)
+        ? letter
+        : convertIndexToLetter(calculateNewIndex(letter, userKey1, userKey2, encodeFlag, currentIndex));
 }
